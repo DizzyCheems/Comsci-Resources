c9090005074f1cdccf1e0d230803ea8de871c405
unknown <stine6595@gmail.com>
2022-12-23 09:05:55 +0800

    updated search

--- ArrayList-Record System/searchArraylistbyName.java
+++ ArrayList-Record System/searchArraylistbyName.java
@@ -1,22 +1,24 @@
-import java.util.ArrayList;
 import java.util.*;
-public class Main {
+import java.util.ArrayList;
+public class Main 
+{
 	public static void main(String[] args)
 	{
         Scanner input = new Scanner(System.in);
 		ArrayList<String> arr = new ArrayList<String>(4);
-
 		arr.add("geeks");
 		arr.add("for");
 		arr.add("geeks");
-
-		String find = input.nextLine();
+        String find = input.nextLine();
 		boolean ans = arr.contains(find);
-
 		if (ans)
+		{
+		    System.out.print("Found Element: ");
 			System.out.println(find);
+		}
 		else
-			System.out.println("Element NOT found");
-
+		{
+			System.out.println("The list does not contain the Element");
+		}
 	}
 }
